c454a55257082e8883cc005e6d4561792fb0fa95
stefanolami <stefanolami90@gmail.com>
2023-06-07 13:13:47 +0200

    done desktop header/landing page

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -26,18 +26,18 @@ export class Header extends TailwindElement(style) {
 
             <!-- TABLET / DESKTOP HEADER -->
 
-            <div class="hidden ${classMap({'md:hidden': this.withImg == true, 'md:flex': this.withImg == false})} justify-between items-center select-none md:bg-transparent pb-2 md:pt-3 md:pb-4 xl:pt-3 xl:pb-7">
+            <div class="hidden ${classMap({'md:hidden': this.withImg == true, 'md:flex': this.withImg == false})} justify-between items-center select-none md:bg-transparent pb-2 md:pt-3 md:pb-4 xl:pt-5 xl:pb-7">
                 <a class="xl:hidden ml-7" href="./home.html">
                     <img class="w-[170px] xl:hidden" src="/images/icons/T&Pgroup-logo.svg" alt="T&P Group logo">
                 </a>
                 <a class="hidden xl:block ml-[4%]" href="./home.html">
-                    <img class="w-[145px]" src="/images/icons/T&Pgroup-logo-desktop.svg" alt="T&P Group logo">             
+                    <img class="w-[126px]" src="/images/icons/T&Pgroup-logo-desktop.svg" alt="T&P Group logo">             
                 </a>
-                <nav id="header-nav" class="flex flex-row justify-around xl:justify-center items-center w-[60%] mr-4 h-full font-jose-400-it text-base text-center">    
-                    <a class="desktop-header-nav-items" href="./welcome.html">Welcome</a>
-                    <a class="desktop-header-nav-items" href="./our-pillars.html">Our Pillars</a>
-                    <a class="desktop-header-nav-items" href="./our-team.html">Our team</a>
-                    <a class="desktop-header-nav-items" href="./contact.html">Contact</a>
+                <nav id="header-nav" class="flex flex-row justify-around xl:justify-center items-center w-[60%] xl:w-2/4 mr-4 h-full font-jose-400-it text-base text-center">    
+                    <a class="xl:border-r border-black xl:w-1/4" href="./welcome.html">Welcome</a>
+                    <a class="xl:border-r border-black xl:w-1/4" href="./our-pillars.html">Our Pillars</a>
+                    <a class="xl:border-r border-black xl:w-1/4" href="./our-team.html">Our team</a>
+                    <a class="xl:w-1/4" href="./contact.html">Contact</a>
                 </nav>
             </div>
 
@@ -46,14 +46,14 @@ export class Header extends TailwindElement(style) {
 
             <div class="hidden ${classMap({'md:hidden': this.withImg == false, 'md:flex': this.withImg == true})}">
                 <div class="w-full">
-                    <a class="block w-[140px] mx-auto mt-12" href="./home.html">
+                    <a class="block w-[140px] xl:w-[212px] mx-auto mt-12" href="./home.html">
                         <img class="w-full" src="/images/icons/T&Pgroup-logo-desktop.svg" alt="T&P Group logo">
                     </a>
-                    <nav id="header-nav" class="flex flex-row justify-center items-center font-jose-400-it text-base text-center text-black mt-10">    
-                        <a class="desktop-header-nav-items px-4 border-r border-r-black" href="./welcome.html">Welcome</a>
-                        <a class="desktop-header-nav-items px-4 border-r border-black" href="./our-pillars.html">Our Pillars</a>
-                        <a class="desktop-header-nav-items px-4 border-r border-black" href="./our-team.html">Our Team</a>
-                        <a class="desktop-header-nav-items px-4" href="./contact.html">Contact</a>
+                    <nav id="header-nav" class="flex flex-row justify-center items-center font-jose-400-it text-base text-center text-black mt-10 xl:mt-24">    
+                        <a class="px-4 xl:px-9 border-r border-r-black" href="./welcome.html">Welcome</a>
+                        <a class="px-4 xl:px-9 border-r border-black" href="./our-pillars.html">Our Pillars</a>
+                        <a class="px-4 xl:px-9 border-r border-black" href="./our-team.html">Our Team</a>
+                        <a class="px-4 xl:px-9" href="./contact.html">Contact</a>
                     </nav>
                 </div>
             </div>
--- src/components/LandingPage.ts
+++ src/components/LandingPage.ts
@@ -10,21 +10,25 @@ export class LandingPage extends TailwindElement(style) {
     render() {
         return html`
             <main>
-                <ul class="flex flex-col justify-center md:flex-row md:flex-wrap items-center mx-auto w-2/5 md:w-2/3 md:mb-24">
+                <div class="hidden xl:block mx-auto text-center mt-20 mb-14">
+                    <h3 class="font-unna-700-it text-2xl mb-6">Welcome to our Group!</h3>
+                    <p class="font-jose-400 text-base">Our network is here to help you get your projects to bloom </br>through our combined expertise!</p>
+                </div>
+                <ul class="flex flex-col justify-center md:flex-row md:flex-wrap items-center mx-auto w-2/5 md:w-2/3 xl:w-[70%] md:mb-24">
                     <img class="w-full md:hidden" src="/images/landing-page/T&Pgroup-logo-big.svg" alt="T&P Group logo">
-                    <a class="block w-full md:w-1/3" href="" target="blank">
+                    <a class="block w-full md:w-1/3 xl:w-1/5" href="" target="blank">
                         <img class="w-full" src="/images/landing-page/T&Pconsulting-logo.svg" alt="T&P Consulting logo">
                     </a>
-                    <a class="block w-full md:w-1/3" href="" target="blank">
+                    <a class="block w-full md:w-1/3 xl:w-1/5" href="" target="blank">
                         <img class="w-full" src="/images/landing-page/T&Pmedia-logo.svg" alt="T&P Media logo">
                     </a>
-                    <a class="block w-full md:w-1/3" href="" target="blank">
+                    <a class="block w-full md:w-1/3 xl:w-1/5" href="" target="blank">
                         <img class="w-full" src="/images/landing-page/T&Pfunding-logo.svg" alt="T&P Funding logo">
                     </a>
-                    <a class="block w-full md:w-1/3" href="" target="blank">
+                    <a class="block w-full md:w-1/3 xl:w-1/5" href="" target="blank">
                         <img class="w-full" src="/images/landing-page/T&Passociations-logo.svg" alt="T&P Associations logo">
                     </a>
-                    <a class="block w-full md:w-1/3 mb-28 mt-6 md:mt-0 md:mb-3" href="" target="blank">
+                    <a class="block w-full md:w-1/3 xl:w-1/5 mb-28 mt-6 md:mt-0 md:mb-3" href="" target="blank">
                         <img class="mx-auto w-3/5" src="/images/landing-page/T&Pinstitute-logo.svg" alt="T&P Institute logo">
                     </a>
                 </ul>    
